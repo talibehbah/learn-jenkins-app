1c14cb1369d729a6dd7c0cc0b53d4df465cf6e34
Chernor Bah <163010973+talibehbah@users.noreply.github.com>
2026-01-10 07:13:29 +0000

    Update app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         </a>
       </header>
       <p>
-          Application version: {process}
+          Application version: {process.env.REACT_APP_VERSION}
       </p>
     </div>
   );
